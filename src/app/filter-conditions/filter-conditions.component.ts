173d27d5ee8269682167809d8b7df2cb64dd825b
Sameh16 <sameh_mohamed16@hotmail.com>
2018-08-08 13:52:08 +0200

    init

---
+++ src/app/filter-conditions/filter-conditions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-conditions',
+  templateUrl: './filter-conditions.component.html',
+  styleUrls: ['./filter-conditions.component.css']
+})
+export class FilterConditionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
