b431c9e5b56eec1fff4373bcff8c822321c252ba
Sameh16 <sameh_mohamed16@hotmail.com>
2018-08-13 01:49:04 +0200

    make div dropped

--- src/app/filter-conditions/filter-conditions.component.ts
+++ src/app/filter-conditions/filter-conditions.component.ts
@@ -6,7 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter-conditions.component.css']
 })
 export class FilterConditionsComponent implements OnInit {
-  conditions = [];
+  choices = [
+    {
+    choice: [
+      {
+        type: 0,	// (0 for meric , 1 for operator)
+        value: 'Running data'	//
+      },
+      {
+        type: 1,	// (0 for meric , 1 for operator)
+        value: '2'	//
+      },
+      {
+        type: null,	// (0 for meric , 1 for operator)
+        value: 'Running data'	//
+      },
+  ],
+    gate: null // (0 for and , 1 for or) gate for next choice
+    },
+    {
+      choice: [
+        {
+          type: 1,	// (0 for meric , 1 for operator)
+          value: '2'	//
+        }
+      ],
+      gate: null // (0 for and , 1 for or) gate for next choice
+      },
+    {
+      choice: [
+        {
+          type: null,	// (0 for meric , 1 for operator)
+          value: 'Running data'	//
+        }
+      ],
+      gate: null // (0 for and , 1 for or) gate for next choice
+      },
+  ];
+
   constructor() { }
 
   ngOnInit() {
@@ -20,7 +57,9 @@ export class FilterConditionsComponent implements OnInit {
     const data = ev.dataTransfer.getData('id');
     const el  = document.getElementById(data);
     ev.target.innerHTML = el.innerHTML;
-
-}
+  }
+  removeMetric($event) {
+    $event.target.classList.toggle('display');
+  }
 
 }
