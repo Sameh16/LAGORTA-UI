96df2459680abf4299a1ad2dbd71bbe184b826f1
Sameh16 <sameh_mohamed16@hotmail.com>
2018-08-14 13:36:45 +0200

    fix one error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {   MatListModule } from '@angular/material';
-//import { RouterModule, Routes} from '@angular/router';
+// import { RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { FilterConditionsComponent } from './filter-conditions/filter-conditions.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
